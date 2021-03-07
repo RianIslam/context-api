0d1bb7f60ed751a040e9f688a4458b07238c7d9c
RianIslam <rianislamdr@gmail.com>
2021-03-08 02:59:50 +0600

    add some code

--- src/components/Catagories/Catagories.js
+++ src/components/Catagories/Catagories.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { CategoryContext } from '../../App'
 import CategoryDetails from '../CategoryDetails/CategoryDetails'
 
-const Catagories = (props) => {
+const allProducts =[{name:'Lenovo',category:'laptop'},{name:'Asus',category:'laptop'},{name:'Hp',category:'laptop'},
+{name:'apple',category:'mobile'},{name:'Nokia',category:'mobile'},{name:'Samsung',category:'mobile'},
+{name:'Nikkon',category:'Camra'},{name:'Canon',category:'Camra'},{name:'Sony',category:'Camra'}
+
+]
+
+
+
+const Catagories = () => {
     
+const category = useContext(CategoryContext)
+const [products,setproducts]  = useState([])
+
+useEffect(() => {
+    const matchProducts = allProducts.filter(pd => pd.category === category && category.toLowerCase())
+    setproducts(matchProducts)
+},[category])
 
     return (
         <div>
-            <h4>select your catagories</h4>
+            <h4>select your catagories :{category}</h4>
+            <h2>
+                {
+                    products.map(pd => <categoryDetails product={pd}/>)
+                }
+            </h2>
             <CategoryDetails />
         </div>
     )
--- src/components/CategoryDetails/CategoryDetails.js
+++ src/components/CategoryDetails/CategoryDetails.js
@@ -3,14 +3,15 @@ import { CategoryContext } from '../../App';
 
 
 
-const CategoryDetails = () => {
+const CategoryDetails = (props) => {
 
+    const {name} = props.product;
     const category = useContext(CategoryContext)
     
     return (
         <div>
             <h5>This is categoryDetails </h5>
-            <h6>Selected Category : {category}</h6>
+            <h6>Selected Product : {name}</h6>
         </div>
     )
 }
